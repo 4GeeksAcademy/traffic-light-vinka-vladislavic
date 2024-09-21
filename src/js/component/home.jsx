2d78c998fe690adb2c208eb968acbc137c07d8ce
Vinka Vladislavic <123342469+VinkaVladislavic@users.noreply.github.com>
2024-09-21 04:08:18 +0000

    traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,28 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [ color, setColor] = useState("red")
+	const [ light, setLight] = useState(["red", "yellow", "green"])
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="traffic-light-container-first">
+			<div className="traffic-light-container-rectangle"></div>
+			<div className="traffic-light-container-second">
+				{light.map((colorLight,index) => <button
+					key={index}
+					onClick={() => setColor(colorLight)}
+					className={`lights btn ${colorLight == color ? "active":"none"}`} 
+					style={{backgroundColor: colorLight}}
+					>
+				</button>)}
+			</div>
+			<button
+				onClick={() => setLight([...light, "purple"])}
+				className="new-color-button">
+			Add color purple
+			</button>
 		</div>
+
 	);
 };
 
